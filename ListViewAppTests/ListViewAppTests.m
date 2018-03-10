9684c8c1aba8d9d9bc6b37fe221063ce2c1e335a
Reetesh <reetesh@ajaykrhansdas-macbook-pro.local>
2018-03-11 02:22:22 +0530

    Implemented Unit Test Case for TableView

--- ListViewAppTests/ListViewAppTests.m
+++ ListViewAppTests/ListViewAppTests.m
@@ -7,8 +7,11 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "ViewController.h"
 
 @interface ListViewAppTests : XCTestCase
+
+@property (nonatomic) ViewController *vc;
 
 @end
 
@@ -17,23 +20,51 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.vc = [[ViewController alloc] init];
+    self.vc.table = [[UITableView alloc] initWithFrame:self.vc.view.bounds style:UITableViewStylePlain];
+    [self.vc.table setDelegate:self.vc];
+    [self.vc.table setDataSource:self.vc];
+    
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.vc = nil;
+    self.vc.table = nil;
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+#pragma mark - View loading tests
+-(void)testThatViewLoads
+{
+    XCTAssertNotNil(self.vc.view, @"View not initiated properly");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+-(void)testThatTableViewLoads
+{
+    XCTAssertNotNil(self.vc.table, @"TableView not initiated");
+}
+
+
+#pragma mark - UITableView tests
+- (void)testThatViewConformsToUITableViewDataSource
+{
+    XCTAssertTrue([self.vc conformsToProtocol:@protocol(UITableViewDataSource) ], @"View does not conform to UITableView datasource protocol");
+}
+
+- (void)testThatTableViewHasDataSource
+{
+    XCTAssertNotNil(self.vc.table.dataSource, @"Table datasource cannot be nil");
+}
+
+- (void)testThatViewConformsToUITableViewDelegate
+{
+    XCTAssertTrue([self.vc conformsToProtocol:@protocol(UITableViewDelegate) ], @"View does not conform to UITableView delegate protocol");
+}
+
+- (void)testTableViewIsConnectedToDelegate
+{
+    XCTAssertNotNil(self.vc.table.delegate, @"Table delegate cannot be nil");
 }
 
 @end
